71d1246bd0b0e49725c1ee2251e9b71e8ecf1eb0
¨Jennifer <jpr.simba@yavirac.edu.ec>
2022-11-28 07:17:07 -0500

    Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { ProductComponent } from './pages/product/product.component';
+import { CatalogueComponent } from './pages/catalogue/catalogue.component';
+import { AppRoutingModule } from './app-routing.module';
+import { PagesComponent } from './pages/pages.component';
 
 @NgModule({
   declarations: [
@@ -21,10 +25,15 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
     HeaderComponent,
     FooterComponent,
     SidebarComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    ProductComponent,
+    CatalogueComponent,
+    PagesComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
